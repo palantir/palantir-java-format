21024f478d2cec9d980da604c49e11be9688504e
Dan Sanduleac <dsanduleac@palantir.com>
2019-10-15 14:26:14 +0100

    damn you compilation order...

--- gradle-palantir-java-format/src/main/java/com/palantir/javaformat/gradle/JavaFormatExtension.java
+++ gradle-palantir-java-format/src/main/java/com/palantir/javaformat/gradle/JavaFormatExtension.java
@@ -5,7 +5,8 @@ import org.gradle.api.provider.Property;
 
 public class JavaFormatExtension {
     private final Property<String> implementationVersion;
-    private static final String IMPLEMENTATION_VERSION = JavaFormatPlugin.class.getPackage().getImplementationVersion();
+    private static final String IMPLEMENTATION_VERSION =
+            JavaFormatExtension.class.getPackage().getImplementationVersion();
 
     public JavaFormatExtension(Project project) {
         implementationVersion = project.getObjects().property(String.class);
