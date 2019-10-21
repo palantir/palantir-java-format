bfd2382462d99bffebbd7fc8bdc61ee29eaae816
Dan Sanduleac <dsanduleac@palantir.com>
2019-10-21 16:00:42 +0100

    docstrings

--- palantir-java-format/src/main/java/com/palantir/javaformat/BreakBehaviour.java
+++ palantir-java-format/src/main/java/com/palantir/javaformat/BreakBehaviour.java
@@ -1,5 +1,7 @@
 package com.palantir.javaformat;
 
+import com.palantir.javaformat.doc.Doc;
+import com.palantir.javaformat.doc.Level;
 import org.derive4j.ArgOption;
 import org.derive4j.Data;
 
@@ -9,8 +11,20 @@ public abstract class BreakBehaviour {
 
         R breakThisLevel();
 
+        /**
+         * If the last level is breakable, prefer breaking it if it will keep the rest of this level on line line.
+         *
+         * @param keepIndentWhenInlined whether to keep this level's indent when inlined as a recursive level (when
+         *     reached via a previous `preferBreakingLastInnerLevel` whose breakability was {@link
+         *     Breakability#CHECK_INNER})
+         */
         R preferBreakingLastInnerLevel(boolean keepIndentWhenInlined);
 
+        /**
+         * Break if by doing so all inner levels then fit on a single line. However, don't break if we can fit in the
+         * {@link Doc docs} up to the first break (which might be nested inside the next doc if it's a {@link Level}),
+         * in order to prevent exceeding the maxLength accidentally.
+         */
         R breakOnlyIfInnerLevelsThenFitOnOneLine(boolean keepIndentWhenInlined);
     }
 
