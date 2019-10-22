8648042466bd3a79993979d0e8db272e873d6528
Dan Sanduleac <dsanduleac@palantir.com>
2019-10-22 14:01:46 +0100

    remove the JavaFormatterOptions argument

--- palantir-java-format/src/main/java/com/palantir/javaformat/java/FormatterServiceImpl.java
+++ palantir-java-format/src/main/java/com/palantir/javaformat/java/FormatterServiceImpl.java
@@ -3,20 +3,27 @@ package com.palantir.javaformat.java;
 import com.google.auto.service.AutoService;
 import com.google.common.collect.ImmutableList;
 import com.google.common.collect.Range;
+import com.palantir.javaformat.java.JavaFormatterOptions.Style;
 import java.util.Collection;
 
 @AutoService(FormatterService.class)
 public final class FormatterServiceImpl implements FormatterService {
 
-    @Override
-    public ImmutableList<Replacement> getFormatReplacements(
-            JavaFormatterOptions options, String text, Collection<Range<Integer>> toRanges) throws FormatterException {
-        return Formatter.createFormatter(options).getFormatReplacements(text, toRanges);
+    private final Formatter formatter;
+
+    public FormatterServiceImpl() {
+        JavaFormatterOptions options = JavaFormatterOptions.builder().style(Style.PALANTIR).build();
+        formatter = Formatter.createFormatter(options);
     }
 
     @Override
-    public String formatSourceReflowStringsAndFixImports(JavaFormatterOptions options, String input)
+    public ImmutableList<Replacement> getFormatReplacements(String text, Collection<Range<Integer>> toRanges)
             throws FormatterException {
-        return Formatter.createFormatter(options).formatSourceAndFixImports(input);
+        return formatter.getFormatReplacements(text, toRanges);
+    }
+
+    @Override
+    public String formatSourceReflowStringsAndFixImports(String input) throws FormatterException {
+        return formatter.formatSourceAndFixImports(input);
     }
 }
