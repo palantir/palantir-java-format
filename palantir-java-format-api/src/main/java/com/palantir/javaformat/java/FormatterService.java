3b9905842d450349b51e12d14a5314d377d8e05b
Dan Sanduleac <dsanduleac@palantir.com>
2019-10-22 13:54:39 +0100

    remove the JavaFormatterOptions argument

--- palantir-java-format-api/src/main/java/com/palantir/javaformat/java/FormatterService.java
+++ palantir-java-format-api/src/main/java/com/palantir/javaformat/java/FormatterService.java
@@ -19,19 +19,18 @@ public interface FormatterService {
      * @throws FormatterException if the input string cannot be parsed
      */
     ImmutableList<Replacement> getFormatReplacements(
-            JavaFormatterOptions options, String input, Collection<Range<Integer>> ranges) throws FormatterException;
+            String input, Collection<Range<Integer>> ranges) throws FormatterException;
 
     /**
      * Formats an input string (a Java compilation unit), reflows strings and fixes imports.
      *
      * <p>Fixing imports includes ordering, spacing, and removal of unused import statements.
      *
-     * @param options the formatting style
      * @param input the input string
      * @return the output string
      * @throws FormatterException if the input string cannot be parsed
      * @see <a href="https://google.github.io/styleguide/javaguide.html#s3.3.3-import-ordering-and-spacing">Google Java
      *     Style Guide - 3.3.3 Import ordering and spacing</a>
      */
-    String formatSourceReflowStringsAndFixImports(JavaFormatterOptions options, String input) throws FormatterException;
+    String formatSourceReflowStringsAndFixImports(String input) throws FormatterException;
 }
