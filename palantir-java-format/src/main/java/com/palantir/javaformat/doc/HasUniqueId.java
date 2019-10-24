70bcc63b25ed11921de76259caed43dbef6ad144
Dan Sanduleac <dsanduleac@palantir.com>
2019-10-24 15:22:38 +0100

    javadoc

--- palantir-java-format/src/main/java/com/palantir/javaformat/doc/HasUniqueId.java
+++ palantir-java-format/src/main/java/com/palantir/javaformat/doc/HasUniqueId.java
@@ -3,6 +3,10 @@ package com.palantir.javaformat.doc;
 import fj.Ord;
 import java.util.concurrent.atomic.AtomicInteger;
 
+/**
+ * This class exists in order to provide a stable {@link Ord} implementation for {@link Doc} and {@link BreakTag}, so
+ * that we can store them inside ordered immutable types like {@link fj.data.Set} and {@link fj.data.HashMap}.
+ */
 abstract class HasUniqueId {
     private static final AtomicInteger UNIQUE_ID_GENERATOR = new AtomicInteger();
 
