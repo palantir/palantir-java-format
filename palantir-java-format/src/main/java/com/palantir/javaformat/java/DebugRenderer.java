3df7756e8752d1a53c6ef16c2b5ec2471f55e071
Dan Fox <dfox@palantir.com>
2019-11-13 15:45:42 +0000

    DebugRenderer does something

--- palantir-java-format/src/main/java/com/palantir/javaformat/java/DebugRenderer.java
+++ palantir-java-format/src/main/java/com/palantir/javaformat/java/DebugRenderer.java
@@ -17,22 +17,87 @@
 package com.palantir.javaformat.java;
 
 import com.google.common.collect.ImmutableList;
+import com.google.common.hash.Hashing;
+import com.palantir.javaformat.CloseOp;
+import com.palantir.javaformat.Input;
 import com.palantir.javaformat.Op;
+import com.palantir.javaformat.OpenOp;
 import com.palantir.javaformat.OpsBuilder;
+import com.palantir.javaformat.doc.Break;
+import com.palantir.javaformat.doc.BreakTag;
 import com.palantir.javaformat.doc.Comment;
+import com.palantir.javaformat.doc.NonBreakingSpace;
+import com.palantir.javaformat.doc.Token;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Optional;
 
 public class DebugRenderer {
 
     public static void render(JavaInput javaInput, OpsBuilder.OpsOutput opsOutput) {
-        // new File("/Users/dfox/Desktop/output.html");
 
-        System.out.println(javaInput.getText());
+        StringBuilder sb = new StringBuilder();
+
+        sb.append("<head>");
+        sb.append("<style type=\"text/css\">"
+                + "span.token:hover { outline: 1px solid black } "
+                + "span.token:hover span.token-body { text-decoration: underline }"
+                + "</style>");
+        sb.append("</head>");
+
+        sb.append("<div style=\"white-space: pre; font-family: monospace\">");
+
+        sb.append("<h1>javaInput</h1>");
+        sb.append("<code>");
+        sb.append(javaInput.getText());
+        sb.append("</code>");
+
+        sb.append("<h1>List&lt;Op&gt;</h1>");
 
         ImmutableList<Op> ops = opsOutput.ops();
         for (Op op : ops) {
-            if (op instanceof Comment) {
-                System.out.println(op);
+            if (op instanceof Token) {
+                long hue = Hashing.adler32().hashInt(((Token) op).uniqueId).padToLong() % 360;
+
+                sb.append(String.format("<span class=\"token\" style=\"background: hsl(%d, 60%%, 90%%)\">", hue));
+                Input.Token foo = ((Token) op).getToken();
+                foo.getToksBefore().forEach(before -> {
+                    sb.append(before.getText());
+                });
+
+                sb.append("<span class=\"token-body\">");
+                sb.append(foo.getTok().getText());
+                sb.append("</span>");
+
+                foo.getToksAfter().forEach(before -> {
+                    sb.append(before.getText());
+                });
+                sb.append("</span>");
             }
+            if (op instanceof Break) {
+                Optional<BreakTag> breakTag = ((Break) op).optTag();
+                // sb.append("break?" + breakTag.map(t -> Integer.toString(t.uniqueId)).orElse(""));
+            }
+            if (op instanceof NonBreakingSpace) {
+                // sb.append("#");
+            }
+            if (op instanceof Comment) {}
+            if (op instanceof OpenOp) {
+                OpenOp openOp = (OpenOp) op;
+            }
+            if (op instanceof CloseOp) {
+                CloseOp closeOp = (CloseOp) op;
+            }
+        }
+
+        sb.append("</div>");
+
+        try {
+            Files.write(Paths.get("/Users/dfox/Desktop/output.html"), sb.toString().getBytes(StandardCharsets.UTF_8));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
         }
     }
 }
