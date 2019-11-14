072e262aa4ae0722e269fc1f5442b5748b3e4e44
Dan Sanduleac <dsanduleac@palantir.com>
2019-11-14 12:16:24 +0000

    get rid of logo and spurious stuff

--- debugger/src/App.tsx
+++ debugger/src/App.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import { Tooltip } from "@blueprintjs/core";
 
@@ -7,7 +6,6 @@ const App: React.FC = () => {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           <Tooltip content={"yep"}>
             <span>Edit <code>src/App.tsx</code> and save to reload!</span>
