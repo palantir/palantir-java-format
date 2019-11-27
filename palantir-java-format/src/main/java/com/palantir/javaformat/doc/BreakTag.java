2d30cff34e5c125a252fe5bc8848affde1338b51
Dan Sanduleac <dsanduleac@palantir.com>
2019-11-27 15:14:23 +0000

    revert BreakTag behaviour so it serializes as {id: number}

--- palantir-java-format/src/main/java/com/palantir/javaformat/doc/BreakTag.java
+++ palantir-java-format/src/main/java/com/palantir/javaformat/doc/BreakTag.java
@@ -1,11 +1,6 @@
 package com.palantir.javaformat.doc;
 
-import com.fasterxml.jackson.core.JsonGenerator;
-import com.fasterxml.jackson.databind.JsonSerializer;
-import com.fasterxml.jackson.databind.SerializerProvider;
-import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import com.google.errorprone.annotations.Immutable;
-import java.io.IOException;
 
 /**
  * Unique identifier for a break. A BreakTag can correspond to one or more {@link Break breaks}, and the state of the
@@ -16,14 +11,4 @@ import java.io.IOException;
  * @see State#wasBreakTaken
  */
 @Immutable
-@JsonSerialize(using = BreakTag.BreakTagSerializer.class)
-public final class BreakTag extends HasUniqueId {
-
-    static class BreakTagSerializer extends JsonSerializer<BreakTag> {
-        @Override
-        public void serialize(
-                BreakTag value, JsonGenerator gen, SerializerProvider serializers) throws IOException {
-            gen.writeString(Integer.toString(value.id()));
-        }
-    }
-}
+public final class BreakTag extends HasUniqueId {}
