7a86d1b962df377ad505f6064af22d975d886710
Dan Sanduleac <dsanduleac@palantir.com>
2019-12-11 16:36:46 +0000

    Add the com.palantir.java-format-spotless plugin

---
+++ gradle-palantir-java-format/src/main/java/com/palantir/javaformat/gradle/PalantirJavaFormatSpotlessPlugin.java
@@ -0,0 +1,24 @@
+package com.palantir.javaformat.gradle;
+
+import com.diffplug.gradle.spotless.SpotlessExtension;
+import com.palantir.javaformat.gradle.spotless.PalantirJavaFormatStep;
+import org.gradle.api.Plugin;
+import org.gradle.api.Project;
+
+public final class PalantirJavaFormatSpotlessPlugin implements Plugin<Project> {
+    @Override
+    public void apply(Project project) {
+        project.getRootProject().getPluginManager().apply(PalantirJavaFormatProviderPlugin.class);
+
+        project.getPluginManager().withPlugin("java", plugin -> {
+            project.getPluginManager().apply("com.diffplug.gradle.spotless");
+
+            SpotlessExtension spotlessExtension = project.getExtensions().getByType(SpotlessExtension.class);
+            spotlessExtension.java(java -> java.addStep(PalantirJavaFormatStep.create(
+                    project.getRootProject()
+                            .getConfigurations()
+                            .getByName(PalantirJavaFormatProviderPlugin.CONFIGURATION_NAME),
+                    project.getRootProject().getExtensions().getByType(JavaFormatExtension.class))));
+        });
+    }
+}
