47619aeae28ccee2c756544319fcf472f0442e24
Dan Sanduleac <dsanduleac@palantir.com>
2019-12-12 13:29:07 +0000

    move spotless stuff to a different class so gradle can decorate the plugin w/o spotless on the classpath

--- gradle-palantir-java-format/src/main/java/com/palantir/javaformat/gradle/PalantirJavaFormatSpotlessPlugin.java
+++ gradle-palantir-java-format/src/main/java/com/palantir/javaformat/gradle/PalantirJavaFormatSpotlessPlugin.java
@@ -1,24 +1,18 @@
 package com.palantir.javaformat.gradle;
 
-import com.diffplug.gradle.spotless.SpotlessExtension;
-import com.palantir.javaformat.gradle.spotless.PalantirJavaFormatStep;
 import org.gradle.api.Plugin;
 import org.gradle.api.Project;
 
-public final class PalantirJavaFormatSpotlessPlugin implements Plugin<Project> {
+public class PalantirJavaFormatSpotlessPlugin implements Plugin<Project> {
     @Override
     public void apply(Project project) {
         project.getRootProject().getPluginManager().apply(PalantirJavaFormatProviderPlugin.class);
 
         project.getPluginManager().withPlugin("java", plugin -> {
             project.getPluginManager().withPlugin("com.diffplug.gradle.spotless", spotlessPlugin -> {
-                SpotlessExtension spotlessExtension = project.getExtensions().getByType(SpotlessExtension.class);
-                spotlessExtension.java(java -> java.addStep(PalantirJavaFormatStep.create(
-                        project.getRootProject()
-                                .getConfigurations()
-                                .getByName(PalantirJavaFormatProviderPlugin.CONFIGURATION_NAME),
-                        project.getRootProject().getExtensions().getByType(JavaFormatExtension.class))));
+                SpotlessInterop.addSpotlessJavaStep(project, PalantirJavaFormatProviderPlugin.CONFIGURATION_NAME);
             });
         });
     }
+
 }
