7d5789563473708b5c7452d8a81894c12e7b1bb6
Dan Sanduleac <dsanduleac@palantir.com>
2019-12-12 17:59:36 +0000

    no need for reflection here

--- gradle-palantir-java-format/src/main/java/com/palantir/javaformat/gradle/PalantirJavaFormatSpotlessPlugin.java
+++ gradle-palantir-java-format/src/main/java/com/palantir/javaformat/gradle/PalantirJavaFormatSpotlessPlugin.java
@@ -1,6 +1,5 @@
 package com.palantir.javaformat.gradle;
 
-import java.lang.reflect.InvocationTargetException;
 import org.gradle.api.Plugin;
 import org.gradle.api.Project;
 
@@ -11,18 +10,7 @@ public class PalantirJavaFormatSpotlessPlugin implements Plugin<Project> {
 
         project.getPluginManager().withPlugin("java", plugin -> {
             project.getPluginManager().withPlugin("com.diffplug.gradle.spotless", spotlessPlugin -> {
-                try {
-                    project.getBuildscript()
-                            .getClassLoader()
-                            .loadClass("com.palantir.javaformat.gradle.SpotlessInterop")
-                            .getDeclaredMethod("addSpotlessJavaStep", Project.class, String.class)
-                            .invoke(null, project, PalantirJavaFormatProviderPlugin.CONFIGURATION_NAME);
-                } catch (IllegalAccessException
-                        | InvocationTargetException
-                        | NoSuchMethodException
-                        | ClassNotFoundException e) {
-                    throw new RuntimeException("Failed to do magic", e);
-                }
+                SpotlessInterop.addSpotlessJavaStep(project, PalantirJavaFormatProviderPlugin.CONFIGURATION_NAME);
             });
         });
     }
