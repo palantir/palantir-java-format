palantir/palantir-java-format

commit 9a52f037c9dd9a1d386b9b4d2ab2dd2ad5e1b2ec

Nested method chains also adhere to column limit (#123)
Nested method chains now also adhere to column limit.

Also, we've made the logic a bit more specific in terms of what are method chains.
We now only apply this restricted column limit to breaks that come before actual method calls, not field accesses or parts of a fully qualified class name.

palantir-java-format/src/main/java/com/palantir/javaformat/doc/Level.java
@@ -123,25 +123,33 @@ public final class Level extends Doc {
      *     maxWidth}
      */
     private Optional<Integer> tryToFitOnOneLine(int maxWidth, State state, Iterable<Doc> docs) {
-        if (getColumnLimitBeforeLastBreak().isPresent()) {
-            float width = 0.0f;
-            float widthBeforeLastBreak = 0.0f;
-            for (Doc doc : docs) {
-                if (doc instanceof Break) {
-                    widthBeforeLastBreak = width;
+        int column = state.column();
+        int columnBeforeLastBreak = 0; // Not activated by default
+        for (Doc doc : docs) {
+            if (doc instanceof Break && ((Break) doc).hasColumnLimit()) {
+                columnBeforeLastBreak = column;
+            } else if (doc instanceof Level) {
+                // Levels might have nested levels that have a 'columnLimitBeforeLastBreak' set, so recurse.
+                State newState = state.withColumn(column);
+                Level innerLevel = (Level) doc;
+                Optional<Integer> newWidth = innerLevel.tryToFitOnOneLine(maxWidth, newState, innerLevel.getDocs());
+                if (!newWidth.isPresent()) {
+                    return Optional.empty();
                 }
-                width += doc.getWidth();
+                column = newWidth.get();
+                continue;
             }
-            // Make an additional check that widthBeforeLastBreak fits in the column limit
-            if (state.column() + widthBeforeLastBreak > getColumnLimitBeforeLastBreak().getAsInt()) {
-                return Optional.empty();
-            }
+            column += doc.getWidth();
+        }
+        // Make an additional check that widthBeforeLastBreak fits in the column limit
+        if (getColumnLimitBeforeLastBreak().isPresent()
+                && columnBeforeLastBreak > getColumnLimitBeforeLastBreak().getAsInt()) {
+            return Optional.empty();
         }
 
         // Check that the entirety of this level fits on the current line.
-        float thisWidth = getWidth(docs);
-        if (state.column() + thisWidth <= maxWidth) {
-            return Optional.of(state.column() + (int) thisWidth);
+        if (column <= maxWidth) {
+            return Optional.of(column);
         }
         return Optional.empty();
     }
@@ -538,6 +546,10 @@ public final class Level extends Doc {
         return openOp;
     }
 
+    /**
+     * An optional, more restrictive column limit for inner breaks that are marked as {@link Break#hasColumnLimit()}. If
+     * the level is to be considered one-lineable, the last such break must not start at a column higher than this.
+     */
     public OptionalInt getColumnLimitBeforeLastBreak() {
         return openOp.columnLimitBeforeLastBreak();
     }

palantir-java-format/src/test/resources/com/palantir/javaformat/java/testdata/B20701054.output
@@ -14,14 +14,37 @@ class B20701054 {
         ImmutableList<String> x = ImmutableList.INSTANCE.add(1).build();
         ImmutableList<String> x = ImmutableList.INSTANCE.add(1).add(2).build();
         ImmutableList<String> x = ImmutableList.INSTANCE.add(1).add(2).add(3).build();
-        ImmutableList<String> x = ImmutableList.INSTANCE.add(1).add(2).add(3).add(4).build();
+        ImmutableList<String> x = ImmutableList.INSTANCE
+                .add(1)
+                .add(2)
+                .add(3)
+                .add(4)
+                .build();
 
         ImmutableList<String> x = ImmutableList.builder().add(1).build();
         ImmutableList<String> x = ImmutableList.builder().add(1).add(2).build();
         ImmutableList<String> x = ImmutableList.builder().add(1).add(2).add(3).build();
-        ImmutableList<String> x = ImmutableList.builder().add(1).add(2).add(3).add(4).build();
-        ImmutableList<String> x = ImmutableList.builder().add(1).add(2).add(3).add(4).add(5).build();
-        ImmutableList<String> x = ImmutableList.builder().add(1).add(2).add(3).add(4).add(5).add(6).build();
+        ImmutableList<String> x = ImmutableList.builder()
+                .add(1)
+                .add(2)
+                .add(3)
+                .add(4)
+                .build();
+        ImmutableList<String> x = ImmutableList.builder()
+                .add(1)
+                .add(2)
+                .add(3)
+                .add(4)
+                .add(5)
+                .build();
+        ImmutableList<String> x = ImmutableList.builder()
+                .add(1)
+                .add(2)
+                .add(3)
+                .add(4)
+                .add(5)
+                .add(6)
+                .build();
 
         ImmutableList<String> x = new ImmutableList.Builder<>()
                 .add(xxxxx)

palantir-java-format/src/test/resources/com/palantir/javaformat/java/testdata/B21465217.output
@@ -4,7 +4,9 @@ class B21465217 {
                 BufferedOutputStream bout = new BufferedOutputStream(out2);
                 OutputStreamWriter writer = new OutputStreamWriter(bout, UTF_8___________________________)) {}
 
-        try (Writer sourceWriter = env.getFiler().createSourceFile(qualifiedNamezzzzzzzz).openWriter()) {
+        try (Writer sourceWriter = env.getFiler()
+                .createSourceFile(qualifiedNamezzzzzzzz)
+                .openWriter()) {
             sourceWriter.append(fileContents);
         }
     }

palantir-java-format/src/test/resources/com/palantir/javaformat/java/testdata/B22610221.output
@@ -1,6 +1,9 @@
 class B22610221 {
     {
-        for (Map.Entry<Key<Object>, Object> entry : FOO.bar().bazs().<Object>thing(someThing).entrySet()) {
+        for (Map.Entry<Key<Object>, Object> entry : FOO.bar()
+                .bazs()
+                .<Object>thing(someThing)
+                .entrySet()) {
             output.put(entry.getKey(), entry.getValue());
         }
     }

palantir-java-format/src/main/java/com/palantir/javaformat/OpsBuilder.java
@@ -438,6 +438,11 @@ public final class OpsBuilder {
         add(Break.make(fillMode, flat, plusIndent, optionalTag));
     }
 
+    /** Emit a self-built {@link Break}. */
+    public void breakOp(Break breakOp) {
+        add(breakOp);
+    }
+
     private int lastPartialFormatBoundary = -1;
 
     /**

palantir-java-format/src/main/java/com/palantir/javaformat/doc/Break.java
@@ -38,6 +38,9 @@ public abstract class Break extends Doc implements Op {
 
     public abstract Optional<BreakTag> optTag();
 
+    /** If this break is restricted by its parent {@link Level}'s {@link Level#getColumnLimitBeforeLastBreak()}. */
+    public abstract boolean hasColumnLimit();
+
     /**
      * Make a {@code Break}.
      *
@@ -47,7 +50,12 @@ public abstract class Break extends Doc implements Op {
      * @return the new {@code Break}
      */
     public static Break make(FillMode fillMode, String flat, Indent plusIndent) {
-        return builder().fillMode(fillMode).flat(flat).plusIndent(plusIndent).build();
+        return builder()
+                .fillMode(fillMode)
+                .flat(flat)
+                .plusIndent(plusIndent)
+                .hasColumnLimit(false)
+                .build();
     }
 
     /**
@@ -65,6 +73,7 @@ public abstract class Break extends Doc implements Op {
                 .flat(flat)
                 .plusIndent(plusIndent)
                 .optTag(optTag)
+                .hasColumnLimit(false)
                 .build();
     }
 
@@ -78,6 +87,7 @@ public abstract class Break extends Doc implements Op {
                 .fillMode(FillMode.FORCED)
                 .flat("")
                 .plusIndent(Indent.Const.ZERO)
+                .hasColumnLimit(false)
                 .build();
     }
 

palantir-java-format/src/main/java/com/palantir/javaformat/java/JavaInputAstVisitor.java
@@ -2727,7 +2727,12 @@ public final class JavaInputAstVisitor extends TreePathScanner<Void, Void> {
         for (ExpressionTree e : items) {
             if (needDot) {
                 if (length > minLength) {
-                    builder.breakOp(FillMode.UNIFIED, "", ZERO);
+                    builder.breakOp(Break.builder()
+                            .fillMode(FillMode.UNIFIED)
+                            .flat("")
+                            .plusIndent(ZERO)
+                            .hasColumnLimit(shouldHaveColumnLimit(e))
+                            .build());
                 }
                 token(".");
                 length++;
@@ -2821,7 +2826,13 @@ public final class JavaInputAstVisitor extends TreePathScanner<Void, Void> {
                     fillMode = FillMode.UNIFIED;
                 }
 
-                builder.breakOp(fillMode, "", ZERO, Optional.of(nameTag));
+                builder.breakOp(Break.builder()
+                        .fillMode(fillMode)
+                        .flat("")
+                        .plusIndent(ZERO)
+                        .optTag(Optional.of(nameTag))
+                        .hasColumnLimit(shouldHaveColumnLimit(e))
+                        .build());
                 token(".");
             }
             BreakTag tyargTag = new BreakTag();
@@ -2839,6 +2850,22 @@ public final class JavaInputAstVisitor extends TreePathScanner<Void, Void> {
         }
 
         builder.close();
+    }
+
+    /**
+     * We only want to limit the max column of expressions that are method invocations. All of these expressions below
+     * would have a column limit.
+     *
+     * <ul>
+     *   <li>{@code foo().bar()}
+     *   <li>{@code foo().bar()[0]}
+     *   <li>{@code foo().bar()[0][0]}
+     * </ul>
+     *
+     * Whereas an expression like a name {@code com.palantir.foo.bar.Baz} would not.
+     */
+    private boolean shouldHaveColumnLimit(ExpressionTree expr) {
+        return getArrayBase(expr).getKind() == METHOD_INVOCATION;
     }
 
     /** Returns the simple names of expressions in a "." chain. */

palantir-java-format/src/test/resources/com/palantir/javaformat/java/testdata/B20128760.output
@@ -11,8 +11,9 @@ class B20128760 {
                 .xxxxx();
 
         xxxxxxXxxx(xxx.xxxXxXxxxxxxx(new XxxxxxxxxXxxxxxxx.Xxxxxxx()
-                                .xxxxXxxxXxx(Xxxxx.xxxXxxxxXxxx(new XxxxXx(
-                                        xxxxxxxxx1.xxxXxxxXx().xxxXxxxx() + xxxxxxxxx2.xxxXxxxXx().xxxXxxxx())))
+                                .xxxxXxxxXxx(Xxxxx.xxxXxxxxXxxx(
+                                        new XxxxXx(xxxxxxxxx1.xxxXxxxXx().xxxXxxxx()
+                                                + xxxxxxxxx2.xxxXxxxXx().xxxXxxxx())))
                                 .xxxxx())
                         .xxxXxxxxxx())
                 .xxxxxxxxXxxxxxx();

commit 15b46e402d7432b218f0c572fa4499a43a1420f3

Fix column limit logic to work as expected with initializers (#138)
Method chains in initializers may once again be laid onto a single next line, if they are short enough. This fixes a regression from #123 (0.3.11) where the column limit for method chains would prevent us from attempting to put the initializer on the 2nd line first and see if that works.

palantir-java-format/src/main/java/com/palantir/javaformat/doc/Level.java
@@ -458,7 +458,7 @@ public final class Level extends Doc {
     }
 
     /** Lay out a Break-separated group of Docs in the current Level. */
-    private static State computeBreakAndSplit(
+    private State computeBreakAndSplit(
             CommentsHelper commentsHelper,
             int maxWidth,
             State state,
@@ -467,9 +467,12 @@ public final class Level extends Doc {
             Obs.ExplorationNode explorationNode) {
         float breakWidth = optBreakDoc.isPresent() ? optBreakDoc.get().getWidth() : 0.0F;
         float splitWidth = getWidth(split);
+
         boolean shouldBreak = (optBreakDoc.isPresent() && optBreakDoc.get().fillMode() == FillMode.UNIFIED)
                 || state.mustBreak()
-                || state.column() + breakWidth + splitWidth > maxWidth;
+                || Double.isInfinite(breakWidth)
+                || !tryToFitOnOneLine(maxWidth, state.withColumn(state.column() + (int) breakWidth), split)
+                        .isPresent();
 
         if (optBreakDoc.isPresent()) {
             state = optBreakDoc.get().computeBreaks(state, shouldBreak);

palantir-java-format/src/test/resources/com/palantir/javaformat/java/testdata/B20701054.output
@@ -14,29 +14,16 @@ class B20701054 {
         ImmutableList<String> x = ImmutableList.INSTANCE.add(1).build();
         ImmutableList<String> x = ImmutableList.INSTANCE.add(1).add(2).build();
         ImmutableList<String> x = ImmutableList.INSTANCE.add(1).add(2).add(3).build();
-        ImmutableList<String> x = ImmutableList.INSTANCE
-                .add(1)
-                .add(2)
-                .add(3)
-                .add(4)
-                .build();
+        ImmutableList<String> x =
+                ImmutableList.INSTANCE.add(1).add(2).add(3).add(4).build();
 
         ImmutableList<String> x = ImmutableList.builder().add(1).build();
         ImmutableList<String> x = ImmutableList.builder().add(1).add(2).build();
         ImmutableList<String> x = ImmutableList.builder().add(1).add(2).add(3).build();
-        ImmutableList<String> x = ImmutableList.builder()
-                .add(1)
-                .add(2)
-                .add(3)
-                .add(4)
-                .build();
-        ImmutableList<String> x = ImmutableList.builder()
-                .add(1)
-                .add(2)
-                .add(3)
-                .add(4)
-                .add(5)
-                .build();
+        ImmutableList<String> x =
+                ImmutableList.builder().add(1).add(2).add(3).add(4).build();
+        ImmutableList<String> x =
+                ImmutableList.builder().add(1).add(2).add(3).add(4).add(5).build();
         ImmutableList<String> x = ImmutableList.builder()
                 .add(1)
                 .add(2)

palantir-java-format/src/test/resources/com/palantir/javaformat/java/testdata/B21465217.output
@@ -4,9 +4,8 @@ class B21465217 {
                 BufferedOutputStream bout = new BufferedOutputStream(out2);
                 OutputStreamWriter writer = new OutputStreamWriter(bout, UTF_8___________________________)) {}
 
-        try (Writer sourceWriter = env.getFiler()
-                .createSourceFile(qualifiedNamezzzzzzzz)
-                .openWriter()) {
+        try (Writer sourceWriter =
+                env.getFiler().createSourceFile(qualifiedNamezzzzzzzz).openWriter()) {
             sourceWriter.append(fileContents);
         }
     }

palantir-java-format/src/test/resources/com/palantir/javaformat/java/testdata/B22610221.output
@@ -1,9 +1,7 @@
 class B22610221 {
     {
-        for (Map.Entry<Key<Object>, Object> entry : FOO.bar()
-                .bazs()
-                .<Object>thing(someThing)
-                .entrySet()) {
+        for (Map.Entry<Key<Object>, Object> entry :
+                FOO.bar().bazs().<Object>thing(someThing).entrySet()) {
             output.put(entry.getKey(), entry.getValue());
         }
     }

palantir-java-format/src/test/resources/com/palantir/javaformat/java/testdata/palantir-chain-next-line.input
@@ -1,0 +1,7 @@
+class PalantirChainNextLine {
+    void foo() {
+        Iterator<Map.Entry<ThingId, DelegateOtherThing>> delegateIterator = delegates
+                .entrySet()
+                .iterator();
+    }
+}

palantir-java-format/src/test/resources/com/palantir/javaformat/java/testdata/palantir-chain-next-line.output
@@ -1,0 +1,6 @@
+class PalantirChainNextLine {
+    void foo() {
+        Iterator<Map.Entry<ThingId, DelegateOtherThing>> delegateIterator =
+                delegates.entrySet().iterator();
+    }
+}