bb286b8c45742ce5f11d07164d874754d61302e6
Dan Sanduleac <dsanduleac@palantir.com>
2020-01-28 16:27:42 +0000

    lol

--- palantir-java-format/src/main/java/com/palantir/javaformat/java/JavaInputAstVisitor.java
+++ palantir-java-format/src/main/java/com/palantir/javaformat/java/JavaInputAstVisitor.java
@@ -688,7 +688,7 @@ public final class JavaInputAstVisitor extends TreePathScanner<Void, Void> {
                 BreakBehaviours.preferBreakingLastInnerLevel(true),
                 node.getClassBody() != null
                         ? LastLevelBreakability.ACCEPT_INLINE_CHAIN
-                        : LastLevelBreakability.CHECK_INNER);￿
+                        : LastLevelBreakability.CHECK_INNER);
         if (node.getEnclosingExpression() != null) {
             scan(node.getEnclosingExpression(), null);
             builder.breakOp();
