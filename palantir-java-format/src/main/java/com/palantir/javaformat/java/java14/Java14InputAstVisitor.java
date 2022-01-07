google/google-java-format

Fix indentation of case statements on JDK 17
Fixes https://github.com/google/google-java-format/issues/643

PiperOrigin-RevId: 392966720

core/src/main/java/com/google/googlejavaformat/java/java14/Java14InputAstVisitor.java
@@ -251,7 +251,7 @@ public class Java14InputAstVisitor extends JavaInputAstVisitor {
       token("default", plusTwo);
     } else {
       token("case", plusTwo);
-      builder.open(plusFour);
+      builder.open(node.getExpressions().size() > 1 ? plusFour : ZERO);
       builder.space();
       boolean first = true;
       for (ExpressionTree expression : node.getExpressions()) {

palantir/palantir-java-format

palantir-java-format/src/main/java/com/palantir/javaformat/java/java14/Java14InputAstVisitor.java
@@ -248,7 +248,7 @@ public class Java14InputAstVisitor extends JavaInputAstVisitor {
         } else {
             token("case", plusTwo);
             builder.space();
-            builder.open(plusTwo);
+            builder.open(node.getExpressions().size() > 1 ? plusTwo : ZERO);
             boolean first = true;
             for (ExpressionTree expression : node.getExpressions()) {
                 if (!first) {