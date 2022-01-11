ba88aa605e13ebb73774069f80328d68ba0d52c3
Carter Kozak <ckozak@palantir.com>
2022-01-11 17:43:14 -0500

    PJF plugin supports the new `PLUGIN_DIR/lib/*.jar` structure
    previous intellij versions used `PLUGIN_DIR/impl/*.jar`

--- idea-plugin/src/main/java/com/palantir/javaformat/intellij/FormatterProvider.java
+++ idea-plugin/src/main/java/com/palantir/javaformat/intellij/FormatterProvider.java
@@ -83,9 +83,13 @@ final class FormatterProvider {
 
     private static List<Path> getBundledImplementationUrls() {
         // Load from the jars bundled with the plugin.
-        Path implDir = PalantirCodeStyleManager.PLUGIN.getPath().toPath().resolve("impl");
-        log.debug("Using palantir-java-format implementation bundled with plugin: {}", implDir);
-        return listDirAsUrlsUnchecked(implDir);
+        Path pluginPath = PalantirCodeStyleManager.PLUGIN.getPluginPath();
+        Path implDir = pluginPath.resolve("impl");
+        // Directory appears to have been renamed from 'impl' to 'lib' in recent idea releases
+        Path pathToUse = Files.isDirectory(implDir) ? implDir : pluginPath.resolve("lib");
+
+        log.debug("Using palantir-java-format implementation bundled with plugin: {}", pathToUse);
+        return listDirAsUrlsUnchecked(pathToUse);
     }
 
     private static List<Path> getImplementationUrls(
