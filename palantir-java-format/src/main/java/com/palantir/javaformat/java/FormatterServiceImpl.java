5011ac211062a4b475fe54e8f7609403ca122a69
Morten Telling <mtelling@palantir.com>
2024-02-21 19:44:14 +0100

    .

--- palantir-java-format/src/main/java/com/palantir/javaformat/java/FormatterServiceImpl.java
+++ palantir-java-format/src/main/java/com/palantir/javaformat/java/FormatterServiceImpl.java
@@ -22,6 +22,8 @@ import com.google.common.collect.Range;
 import com.palantir.javaformat.java.JavaFormatterOptions.Style;
 import java.util.Collection;
 import java.util.concurrent.TimeUnit;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 @AutoService(FormatterService.class)
 public final class FormatterServiceImpl implements FormatterService {
@@ -45,8 +47,17 @@ public final class FormatterServiceImpl implements FormatterService {
         Stopwatch stopwatch = Stopwatch.createStarted();
         String output = formatter.formatSourceAndFixImports(input);
         stopwatch.stop();
+
         if (stopwatch.elapsed(TimeUnit.MILLISECONDS) > 200) {
-            throw new RuntimeException("Spent more than 200 millis on formatting file: " + input);
+            Pattern pattern = Pattern.compile("class\\s+.*\\s");
+            Matcher matcher = pattern.matcher(output);
+            if (matcher.find()) {
+                throw new RuntimeException("Spent more than 200 millis on formatting file: " + matcher.group()
+                        + " duration: " + stopwatch.elapsed(TimeUnit.MILLISECONDS));
+            } else {
+                throw new RuntimeException("Spent more than 200 millis on formatting unknown file: " + matcher.group()
+                        + " duration: " + stopwatch.elapsed(TimeUnit.MILLISECONDS));
+            }
         }
         return output;
     }
