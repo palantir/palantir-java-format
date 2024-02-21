6304a43018aeaf43605948dc0c2cae5795f751a4
Morten Telling <mtelling@palantir.com>
2024-02-21 19:28:28 +0100

    .

--- palantir-java-format/src/main/java/com/palantir/javaformat/java/FormatterServiceImpl.java
+++ palantir-java-format/src/main/java/com/palantir/javaformat/java/FormatterServiceImpl.java
@@ -18,28 +18,27 @@ package com.palantir.javaformat.java;
 import com.google.auto.service.AutoService;
 import com.google.common.collect.ImmutableList;
 import com.google.common.collect.Range;
-import com.palantir.javaformat.java.JavaFormatterOptions.Style;
 import java.util.Collection;
 
 @AutoService(FormatterService.class)
 public final class FormatterServiceImpl implements FormatterService {
 
-    private final Formatter formatter;
-
     public FormatterServiceImpl() {
-        JavaFormatterOptions options =
-                JavaFormatterOptions.builder().style(Style.PALANTIR).build();
-        formatter = Formatter.createFormatter(options);
+        //        JavaFormatterOptions options =
+        //                JavaFormatterOptions.builder().style(Style.PALANTIR).build();
+        //        Formatter formatter = Formatter.createFormatter(options);
     }
 
     @Override
     public ImmutableList<Replacement> getFormatReplacements(String text, Collection<Range<Integer>> toRanges)
             throws FormatterException {
-        return formatter.getFormatReplacements(text, toRanges);
+        throw new RuntimeException("Hit the right format service impl");
+        //            return formatter.getFormatReplacements(text, toRanges);
     }
 
     @Override
     public String formatSourceReflowStringsAndFixImports(String input) throws FormatterException {
-        return formatter.formatSourceAndFixImports(input);
+        throw new RuntimeException("Hit the right format service impl - on fix imports path");
+        //        return formatter.formatSourceAndFixImports(input);
     }
 }
