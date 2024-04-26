678a999c748eafbb44826a12424b802eeb1eea58
Claudia Rogoz <crogoz@palantir.com>
2024-04-26 12:16:59 +0200

    add import tests

--- idea-plugin/src/main/java/com/palantir/javaformat/intellij/PalantirJavaFormatFormattingService.java
+++ idea-plugin/src/main/java/com/palantir/javaformat/intellij/PalantirJavaFormatFormattingService.java
@@ -37,11 +37,8 @@ import java.util.List;
 import java.util.Optional;
 import java.util.Set;
 import org.jetbrains.annotations.NotNull;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 class PalantirJavaFormatFormattingService extends AsyncDocumentFormattingService {
-    private static final Logger log = LoggerFactory.getLogger(PalantirJavaFormatFormattingService.class);
     private final FormatterProvider formatterProvider = new FormatterProvider();
 
     @Override
