d44718e8de71081eeacba7bc16d7b4082e8093ca
Callum Rogers <callumr@palantir.com>
2024-04-26 16:03:21 +0100

    Fix compilation error

--- idea-plugin/src/main/java/com/palantir/javaformat/intellij/FormatterProvider.java
+++ idea-plugin/src/main/java/com/palantir/javaformat/intellij/FormatterProvider.java
@@ -19,7 +19,11 @@ package com.palantir.javaformat.intellij;
 import com.github.benmanes.caffeine.cache.Caffeine;
 import com.github.benmanes.caffeine.cache.LoadingCache;
 import com.google.common.annotations.VisibleForTesting;
+import com.google.common.base.Preconditions;
+import com.intellij.ide.plugins.IdeaPluginDescriptor;
+import com.intellij.ide.plugins.PluginManager;
 import com.intellij.openapi.application.ApplicationInfo;
+import com.intellij.openapi.extensions.PluginId;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.projectRoots.JdkUtil;
 import com.intellij.openapi.projectRoots.Sdk;
@@ -28,7 +32,6 @@ import com.intellij.openapi.util.BuildNumber;
 import com.intellij.openapi.util.SystemInfo;
 import com.palantir.javaformat.bootstrap.BootstrappingFormatterService;
 import com.palantir.javaformat.java.FormatterService;
-import com.palantir.logsafe.Preconditions;
 import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URI;
@@ -49,6 +52,12 @@ import org.slf4j.LoggerFactory;
 
 final class FormatterProvider {
     private static final Logger log = LoggerFactory.getLogger(FormatterProvider.class);
+
+    private static final String PLUGIN_ID = "palantir-java-format";
+    static final IdeaPluginDescriptor PLUGIN = Preconditions.checkNotNull(
+            PluginManager.getPlugin(PluginId.getId(PLUGIN_ID)),
+            "Couldn't find our own plugin: %s",
+            PLUGIN_ID);
 
     // Cache to avoid creating a URLClassloader every time we want to format from IntelliJ
     private final LoadingCache<FormatterCacheKey, Optional<FormatterService>> implementationCache =
@@ -104,7 +113,7 @@ final class FormatterProvider {
 
     private static List<Path> getBundledImplementationUrls() {
         // Load from the jars bundled with the plugin.
-        Path implDir = PalantirCodeStyleManager.PLUGIN.getPath().toPath().resolve("impl");
+        Path implDir = PLUGIN.getPath().toPath().resolve("impl");
         log.debug("Using palantir-java-format implementation bundled with plugin: {}", implDir);
         return listDirAsUrlsUnchecked(implDir);
     }
