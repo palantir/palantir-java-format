b074a0a7c4f192a56cae19c4d89bcfee72a38f10
Kelvin Ou <kelvin.ou.jin.bin@gmail.com>
2025-06-12 15:42:46 +0100

    Add a detailed javadoc

--- gradle-palantir-java-format/src/test/groovy/com/palantir/javaformat/gradle/SupportsSpotless622.groovy
+++ gradle-palantir-java-format/src/test/groovy/com/palantir/javaformat/gradle/SupportsSpotless622.groovy
@@ -17,6 +17,16 @@ package com.palantir.javaformat.gradle
 
 import nebula.test.IntegrationTestKitSpec
 
+/**
+ * When we were getting gradle-baseline to support the configuration cache, spotless had some poorly written tasks
+ * which caused issues with the configuration cache.
+ *
+ * Bumping spotless to 6.22.0 fixed this, but revealed a new error — the palantirJavaFormat configuration was
+ * being resolved eagerly: https://github.com/palantir/palantir-java-format/blob/b7b5995df3be690780939c0d0cb2ec49b99c68c8/gradle-palantir-java-format/src/main/java/com/palantir/javaformat/gradle/spotless/NativePalantirJavaFormatStep.java#L45
+ * gradle-consistent-versions enforces against resolving configurations at configuration time, and throws an error.
+ *
+ * This test forces creation of the spotless steps, which will reveal any eager resolution of configurations.
+ */
 class SupportsSpotless622 extends IntegrationTestKitSpec {
     private static final CLASSPATH_FILE = new File("build/impl.classpath").absolutePath
 
