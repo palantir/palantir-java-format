d746a44d92bd200f5e1ce746e24afd814aa8ea56
Finlay Williams <finlayw@palantir.com>
2025-12-16 10:47:13 +0000

    add constraint

--- gradle-palantir-java-format/src/test/java/com/palantir/javaformat/gradle/PalantirJavaFormatSpotlessPluginTest.java
+++ gradle-palantir-java-format/src/test/java/com/palantir/javaformat/gradle/PalantirJavaFormatSpotlessPluginTest.java
@@ -68,6 +68,14 @@ class PalantirJavaFormatSpotlessPluginTest {
                 .add("com.palantir.jdks.latest");
 
         project.buildGradle().append("""
+                buildscript {
+                    dependencies {
+                        constraints {
+                            classpath 'com.diffplug.spotless:6.22.0'
+                        }
+                    }
+                }
+                
                 javaVersions {
                     libraryTarget = %s
                 }
