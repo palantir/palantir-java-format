89411333709afe1dfdbcf74ae47a92d29a79ea17
Dan Herlihy <dherlihy@palantir.com>
2026-05-20 12:25:39 -0400

    Cleaner test

--- palantir-java-format/src/test/java/com/palantir/javaformat/java/FormatterTest.java
+++ palantir-java-format/src/test/java/com/palantir/javaformat/java/FormatterTest.java
@@ -432,19 +432,18 @@ public final class FormatterTest {
 
     @Test
     public void wrapMarkdownDocstringComment() throws Exception {
-        assertThat(Formatter.create()
-                        .formatSource("class T {\n"
-                                + "  /// one long incredibly"
-                                + " unbroken sentence moving from topic to topic so that no-one had a"
-                                + " chance to interrupt;\n"
-                                + "  void m() {}\n"
-                                + "}\n"))
-                .isEqualTo("class T {\n"
-                        + "  /// one long incredibly unbroken sentence moving from topic to topic so that"
-                        + " no-one had a chance\n"
-                        + "  /// to interrupt;\n"
-                        + "  void m() {}\n"
-                        + "}\n");
+        String input = "class T {\n"
+                + "  /// one long incredibly unbroken sentence moving from topic to topic so that no-one"
+                + " had a chance to interrupt;\n"
+                + "  void m() {}\n"
+                + "}\n";
+        String expected = "class T {\n"
+                + "  /// one long incredibly unbroken sentence moving from topic to topic so that no-one"
+                + " had a chance\n"
+                + "  /// to interrupt;\n"
+                + "  void m() {}\n"
+                + "}\n";
+        assertThat(Formatter.create().formatSource(input)).isEqualTo(expected);
     }
 
     @Test
